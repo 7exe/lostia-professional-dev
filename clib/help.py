df84f88bca1bb4646a9ebc82947b903d459017ac
Stonkic <asimo.developer@outlook.com>
2020-08-14 19:13:29 +0000

    added date command, help command bug fixes

--- clib/help.py
+++ clib/help.py
@@ -15,51 +15,51 @@ for command in CoreCommands:
 doPrint = False
 
 def MainCommand():
-  os.system("clear")
   def do_the_actuall_command():
     global doPrint
     if(len(sys.argv) > 1):
       if(os.path.exists("LostiaHelp/"+sys.argv[1]+".help")):
+        curlString = ""
         for I in Modules:
           if(I.replace("\n","") == sys.argv[1]):
             Help = open("LostiaHelp/"+sys.argv[1]+".help").readlines()
   
             for I in Help:
               if(len(I.split("<name>"))==3):
-                print("NAME")
-                print("       "+I.split("<name>")[1].replace("\l","\n")+"\n")
+                curlString+="NAME\n"
+                curlString+="       "+I.split("<name>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<synopsis>"))==3):
-                print("SYNOPSIS")
-                print("       "+I.split("<synopsis>")[1].replace("\l","\n")+"\n")
+                curlString+="SYNOPSIS\n"
+                curlString+="       "+I.split("<synopsis>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<description>"))==3):
-                print("DESCRIPTION")
-                print("       "+I.split("<description>")[1].replace("\l","\n")+"\n")
+                curlString+="DESCRIPTION\n"
+                curlString+="       "+I.split("<description>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<options>"))==3):
-                print("OPTIONS")
-                print("       "+I.split("<options>")[1].replace("\l","\n")+"\n")
+                curlString+="OPTIONS\n"
+                curlString+="       "+I.split("<options>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<seealso>"))==3):
-                print("SEE ALSO")
-                print("       "+I.split("<seealso>")[1].replace("\l","\n")+"\n")
+                curlString+="SEE ALSO\n"
+                curlString+="       "+I.split("<seealso>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<exitstatus>"))==3):
-                print("EXIT STATUS")
-                print("       "+I.split("<exitstatu>")[1].replace("\l","\n")+"\n")
+                curlString+="EXIT STATUS\n"
+                curlString+="       "+I.split("<exitstatu>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<examples>"))==3):
-                print("EXAMPLES")
-                print("       "+I.split("<examples>")[1].replace("\l","\n")+"\n")
+                curlString+="EXAMPLES\n"
+                curlString+="       "+I.split("<examples>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<history>"))==3):
-                print("HISTORY")
-                print("       "+I.split("<history>")[1].replace("\l","\n")+"\n")
+                curlString+="HISTORY\n"
+                curlString+="       "+I.split("<history>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<bugs>"))==3):
-                print("BUGS")
-                print("       "+I.split("<bugs>")[1].replace("\l","\n")+"\n")
+                curlString+="BUGS\n"
+                curlString+="       "+I.split("<bugs>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<standards>"))==3):
-                print("STANDARDS")
-                print("       "+I.split("<standards>")[1].replace("\l","\n")+"\n")
+                curlString+="STANDARDS\n"
+                curlString+="       "+I.split("<standards>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<exitvalues>"))==3):
-                print("EXIT VALUES")
-                print("       "+I.split("<exitvalues>")[1].replace("\l","\n")+"\n")
+                curlString+="EXIT VALUES\n"
+                curlString+="       "+I.split("<exitvalues>")[1].replace("\l","\n")+"\n"
               #.replace("#BLUE","\033[38;5;27m").replace("#RESET","\033[39m")
-            print("\n\x1b[6;30;47m(END)\n\x1b[0m")
+            curlString+="\n\x1b[6;30;47m(END)\n\x1b[0m"
             doPrint = True
           else:
             pass
@@ -68,50 +68,54 @@ def MainCommand():
             Help = open("LostiaHelp/"+sys.argv[1]+".help").readlines()
             for I in Help:
               if(len(I.split("<name>"))==3):
-                print("NAME")
-                print("       "+I.split("<name>")[1].replace("\l","\n")+"\n")
+                curlString+="NAME\n"
+                curlString+="       "+I.split("<name>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<synopsis>"))==3):
-                print("SYNOPSIS")
-                print("       "+I.split("<synopsis>")[1].replace("\l","\n")+"\n")
+                curlString+="SYNOPSIS\n"
+                curlString+="       "+I.split("<synopsis>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<description>"))==3):
-                print("DESCRIPTION")
-                print("       "+I.split("<description>")[1].replace("\l","\n")+"\n")
+                curlString+="DESCRIPTION\n"
+                curlString+="       "+I.split("<description>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<options>"))==3):
-                print("OPTIONS")
-                print("       "+I.split("<options>")[1].replace("\l","\n")+"\n")
+                curlString+="OPTIONS\n"
+                curlString+="       "+I.split("<options>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<seealso>"))==3):
-                print("SEE ALSO")
-                print("       "+I.split("<seealso>")[1].replace("\l","\n")+"\n")
+                curlString+="SEE ALSO\n"
+                curlString+="       "+I.split("<seealso>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<exitstatus>"))==3):
-                print("EXIT STATUS")
-                print("       "+I.split("<exitstatu>")[1].replace("\l","\n")+"\n")
+                curlString+="EXIT STATUS\n"
+                curlString+="       "+I.split("<exitstatu>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<examples>"))==3):
-                print("EXAMPLES")
-                print("       "+I.split("<examples>")[1].replace("\l","\n")+"\n")
+                curlString+="EXAMPLES\n"
+                curlString+="       "+I.split("<examples>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<history>"))==3):
-                print("HISTORY")
-                print("       "+I.split("<history>")[1].replace("\l","\n")+"\n")
+                curlString+="HISTORY\n"
+                curlString+="       "+I.split("<history>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<bugs>"))==3):
-                print("BUGS")
-                print("       "+I.split("<bugs>")[1].replace("\l","\n")+"\n")
+                curlString+="BUGS\n"
+                curlString+="       "+I.split("<bugs>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<standards>"))==3):
-                print("STANDARDS")
-                print("       "+I.split("<standards>")[1].replace("\l","\n")+"\n")
+                curlString+="STANDARDS\n"
+                curlString+="       "+I.split("<standards>")[1].replace("\l","\n")+"\n"
               if(len(I.split("<exitvalues>"))==3):
-                print("EXIT VALUES")
-                print("       "+I.split("<exitvalues>")[1].replace("\l","\n")+"\n")
+                curlString+="EXIT VALUES\n"
+                curlString+="       "+I.split("<exitvalues>")[1].replace("\l","\n")+"\n"
               #.replace("#BLUE","\033[38;5;27m").replace("#RESET","\033[39m")
               #.replace("#BLUE","\033[38;5;27m").replace("#RESET","\033[39m").replace("#PINK","\033[38;5;201m").replace("#RED","\033[38;5;1m").replace("#YELLOW","\033[38;5;11m")
-            print("\x1b[6;30;47m(END)\x1b[0m")
+            curlString+="\x1b[6;30;47m(END)\x1b[0m"
             doPrint = True
           else:
             pass
         if(doPrint != True):
           print("No help entry for "+sys.argv[1])
+        else:
+          os.system("clear")
+          print(curlString)
         
   
       else:
         print("No help entry for "+sys.argv[1])
+      
     
     else:
       folder = "LostiaHelp/"
